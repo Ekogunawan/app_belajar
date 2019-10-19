f031e5e4238da62f9d87c2091c001e7922af17a7
Ekogunawan <48266816+Ekogunawan@users.noreply.github.com>
2019-10-19 23:02:06 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/gmail/akakom16/eko/app_belajar/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.gmail.akakom16.eko.app_belajar
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
